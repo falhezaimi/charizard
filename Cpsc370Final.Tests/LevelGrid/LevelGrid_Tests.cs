8c26c1c0aec5af317b22a20d19e51f2dc68db2a9
Cristian Melgoza <cpmelgoza05@gmail.com>
2025-01-29 15:15:50 -0800

    Create test for LevelGrid IsPositionInBounds()

--- Cpsc370Final.Tests/LevelGrid/LevelGrid_Tests.cs
+++ Cpsc370Final.Tests/LevelGrid/LevelGrid_Tests.cs
@@ -2,5 +2,30 @@
 
 public class LevelGrid_Tests
 {
-    
+    [Fact]
+    private void TestIsPositionInBounds()
+    {
+        LevelGrid levelGrid = new LevelGrid(20, 10);
+        List<GridPosition> gridPositionsInBounds = new List<GridPosition>();
+        gridPositionsInBounds.Add(new GridPosition(0, 0));
+        gridPositionsInBounds.Add(new GridPosition(19, 0));
+        gridPositionsInBounds.Add(new GridPosition(0, 9));
+        gridPositionsInBounds.Add(new GridPosition(19, 9));
+
+        List<GridPosition> gridPositionsNotInBounds = new List<GridPosition>();
+        gridPositionsNotInBounds.Add(new GridPosition(-1, 0));
+        gridPositionsNotInBounds.Add(new GridPosition(0, -1));
+        gridPositionsNotInBounds.Add(new GridPosition(20, 0));
+        gridPositionsNotInBounds.Add(new GridPosition(0, 10));
+
+        foreach (GridPosition gridPosition in gridPositionsInBounds)
+        {
+            Assert.True(levelGrid.IsPositionInBounds(gridPosition));
+        }
+
+        foreach (GridPosition gridPosition in gridPositionsNotInBounds)
+        {
+            Assert.False(levelGrid.IsPositionInBounds(gridPosition));
+        }
+    }
 }
