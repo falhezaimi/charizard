1d18dc119e33d86b89b48af43e138a558ed7e332
Cristian Melgoza <cpmelgoza05@gmail.com>
2025-01-29 11:15:03 -0800

    Refactor grid creation and rendering to LevelGrid

---
+++ Cpsc370Final/LevelGrid.cs
@@ -0,0 +1,75 @@
+﻿namespace Cpsc370Final;
+
+public class LevelGrid
+{
+    private static GameObject[,] levelGrid;
+    private static List<GameObject> gameObjects = new List<GameObject>();
+
+    public LevelGrid(int width, int height)
+    {
+        levelGrid = new GameObject[height, width];
+        for (int x = 0; x < GetWidth(); x++)
+        {
+            for (int y = 0; y < GetHeight(); y++)
+            {
+                SetGameObjectAtPosition(x, y, null);
+            }
+        }
+    }
+
+    public GameObject GetGameObjectAtPosition(int x, int y)
+    {
+        return levelGrid[y, x];
+    }
+
+    private void SetGameObjectAtPosition(int x, int y, GameObject gameObject)
+    {
+        levelGrid[y, x] = gameObject;
+    }
+
+    public void Render()
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write("/  ");
+        for (int i = 0; i < GetWidth(); i++)
+        {
+            Console.Write("-  ");
+        }
+        Console.WriteLine("\\");
+        
+        for (int y = 0; y < GetHeight(); y++)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|  ");
+            for (int x = 0; x < GetWidth(); x++)
+            {
+                GameObject gameObject = levelGrid[y, x];
+                if (gameObject != null)
+                {
+                    Console.ForegroundColor = gameObject.GetAsciiColor();
+                    Console.Write(gameObject.GetAsciiCharacter());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.Write('.');
+                }
+                Console.Write("  ");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("|");
+            Console.WriteLine();
+        }
+        
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write("\\  ");
+        for (int i = 0; i < GetWidth(); i++)
+        {
+            Console.Write("-  ");
+        }
+        Console.Write("/");
+    }
+    
+    public int GetWidth() => levelGrid.GetLength(1);
+    public int GetHeight() => levelGrid.GetLength(0);
+}
--- Cpsc370Final/Program.cs
+++ Cpsc370Final/Program.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-    private static GameObject[,] worldGrid;
+    private static LevelGrid levelGrid;
     private static List<GameObject> gameObjects = new List<GameObject>();
     private static Random rand = new Random();
 
@@ -32,37 +32,30 @@ class Program
     
     private static void GenerateMap()
     {
-        worldGrid = new GameObject[10, 20];
-        for (int i = 0; i < worldGrid.GetLength(0); i++)
-        {
-            for (int j = 0; j < worldGrid.GetLength(1); j++)
-            {
-                worldGrid[i, j] = null;
-            }
-        }
+        levelGrid = new LevelGrid(20, 10);
         
-        SpawnPlayer();
-        SpawnGoblins();
+        //SpawnPlayer();
+        //SpawnGoblins();
     }
 
-    private static void SpawnPlayer()
-    {
-        int middleX = (worldGrid.GetLength(1) - 1) / 2;
-        int middleY = (worldGrid.GetLength(0) - 1) / 2;
-        player = new Player(worldGrid, middleX, middleY);
-        gameObjects.Add(player);
-    }
-
-    private static void SpawnGoblins()
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            int randomX = rand.Next(0, worldGrid.GetLength(1) - 1);
-            int randomY = rand.Next(0, worldGrid.GetLength(0) - 1);
-            GameObject goblin = new Goblin(worldGrid, randomX, randomY);
-            gameObjects.Add(goblin);
-        }
-    }
+    // private static void SpawnPlayer()
+    // {
+    //     int middleX = (levelGrid.GetWidth() - 1) / 2;
+    //     int middleY = (levelGrid.GetHeight() - 1) / 2;
+    //     player = new Player(levelGrid, middleX, middleY);
+    //     gameObjects.Add(player);
+    // }
+    //
+    // private static void SpawnGoblins()
+    // {
+    //     for (int i = 0; i < 5; i++)
+    //     {
+    //         int randomX = rand.Next(0, levelGrid.GetWidth() - 1);
+    //         int randomY = rand.Next(0, levelGrid.GetHeight() - 1);
+    //         GameObject goblin = new Goblin(levelGrid, randomX, randomY);
+    //         gameObjects.Add(goblin);
+    //     }
+    // }
 
     private static void ProcessCommand(string command)
     {
@@ -77,47 +70,7 @@ class Program
     
     private static void Render() {
         Console.Clear();
-        
         Console.WriteLine("Game Title:\n");
-        
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.Write("/  ");
-        for (int i = 0; i < worldGrid.GetLength(1); i++)
-        {
-            Console.Write("-  ");
-        }
-        Console.WriteLine("\\");
-        
-        for (int i = 0; i < worldGrid.GetLength(0); i++)
-        {
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write("|  ");
-            for (int j = 0; j < worldGrid.GetLength(1); j++)
-            {
-                GameObject gameObject = worldGrid[i, j];
-                if (gameObject != null)
-                {
-                    Console.ForegroundColor = gameObject.GetAsciiColor();
-                    Console.Write(gameObject.GetAsciiCharacter());
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.Write('.');
-                }
-                Console.Write("  ");
-            }
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write("|");
-            Console.WriteLine();
-        }
-        
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.Write("\\  ");
-        for (int i = 0; i < worldGrid.GetLength(1); i++)
-        {
-            Console.Write("-  ");
-        }
-        Console.Write("/");
+        levelGrid.Render();
     }
 }
