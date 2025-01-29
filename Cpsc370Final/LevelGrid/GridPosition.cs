4ceeb9380c6aff5ce2ff948352da32cccbc2bd07
Cristian Melgoza <cpmelgoza05@gmail.com>
2025-01-29 11:47:34 -0800

    Add GridPosition struct

---
+++ Cpsc370Final/LevelGrid/GridPosition.cs
@@ -0,0 +1,61 @@
+﻿namespace Cpsc370Final;
+
+public struct GridPosition : IEquatable<GridPosition>
+{
+    public int x;
+    public int y;
+
+    public GridPosition(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static float Distance(GridPosition a, GridPosition b)
+    {
+        int distanceX = Math.Abs(a.x - b.x);
+        int distanceZ = Math.Abs(a.y - b.y);
+        int distance = distanceX + distanceZ;
+        return distance;
+    }
+
+    public override string ToString()
+    {
+        return $"({x}, {y})";
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is GridPosition gridPosition && gridPosition == this;
+    }
+
+    public bool Equals(GridPosition other)
+    {
+        return this == other;
+    }
+
+    public override readonly int GetHashCode()
+    {
+        return HashCode.Combine(x, y);
+    }
+
+    public static GridPosition operator +(GridPosition a, GridPosition b)
+    {
+        return new GridPosition(a.x + b.x, a.y + b.y);
+    }
+
+    public static GridPosition operator -(GridPosition a, GridPosition b)
+    {
+        return new GridPosition(a.x - b.x, a.y - b.y);
+    }
+
+    public static bool operator ==(GridPosition a, GridPosition b)
+    {
+        return a.x == b.x && a.y == b.y;
+    }
+
+    public static bool operator !=(GridPosition a, GridPosition b)
+    {
+        return a.x != b.x || a.y != b.y;
+    }
+}
