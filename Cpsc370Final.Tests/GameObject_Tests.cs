b73fec42cbb4d42c6c4a1aad8638070cc377b345
Cristian Melgoza <cpmelgoza05@gmail.com>
2025-01-30 08:53:25 -0800

    Create GameObject tests for player interaction

---
+++ Cpsc370Final.Tests/GameObject_Tests.cs
@@ -0,0 +1,33 @@
+﻿namespace Cpsc370Final.Tests;
+using Cpsc370Final.Core;
+using Cpsc370Final.Entities;
+using Cpsc370Final.Objects;
+
+public class GameObject_Tests
+{
+    [Fact]
+    private void TestInteractionTriggeredByPlayer()
+    {
+        LevelGrid levelGrid = new LevelGrid(5, 5);
+        Player player = new Player(levelGrid, new GridPosition(0, 0));
+        Key key =  new Key(levelGrid, new GridPosition(1, 0));
+        Goblin goblin = new Goblin(levelGrid, new GridPosition(2, 0));
+        
+        player.Move(Direction.East);
+        Assert.True(player.position == new GridPosition(1, 0));
+        Assert.True(player.HeldKeys == 1);
+        Assert.DoesNotContain(key, levelGrid.GetGameObjects());
+    }
+
+    [Fact]
+    private void TestInteractionTriggeredByEnemy()
+    {
+        LevelGrid levelGrid = new LevelGrid(5, 5);
+        Player player = new Player(levelGrid, new GridPosition(0, 0));
+        Goblin goblin = new Goblin(levelGrid, new GridPosition(1, 0));
+        goblin.Move(Direction.West);
+        
+        Assert.True(goblin.position == new GridPosition(0, 0));
+        Assert.DoesNotContain(player, levelGrid.GetGameObjects());
+    }
+}
